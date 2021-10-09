95fe8a77cdbeef065b2f8cce0bfbca7a109af23b
Abner Davi de Souza Miranda <abner_davi@ucl.br>
2021-10-09 00:23:31 -0300

    criação da classe Complexo

---
+++ imaginum/imaginum/Complexos.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace imaginum
+{
+    class Complexos
+    {
+       public double real { get; private set; }
+       public double imaginario { get; private set; }
+
+       public Complexos() {}
+       public Complexos(double _real, double _imaginario)
+       {
+           real = _real;
+           imaginario =_imaginario;
+       }
+
+    }
+}
