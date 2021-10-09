2c8e3e3bb7dfea5e2ac77914c66b85ee9be5d108
Abner Davi de Souza Miranda <abner_davi@ucl.br>
2021-10-08 21:53:21 -0300

    Criação do projeto em C# no Visual Studio

---
+++ imaginum/imaginum/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace imaginum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
