d20b75c163020fc98590500ec9e2662a567d9245
Abner Davi de Souza Miranda <abner_davi@ucl.br>
2021-10-09 15:15:46 -0300

    primeira versão funcional

--- imaginum/imaginum/Main.cs
+++ imaginum/imaginum/Main.cs
@@ -8,28 +8,71 @@ namespace imaginum
             int op;
             Console.WriteLine("----------Bem vindo a Imaginum----------");
             Console.WriteLine("---Calculadora de numeros imaginarios---");
-            Console.WriteLine("> 1 - Adição ");
-            Console.WriteLine("> 2 - Subtração");
-            Console.WriteLine("> 3 - Multiplicação");
-            Console.WriteLine("> 4 - Divisão");
-            Console.WriteLine("> 5 - Módulo");
-            Console.WriteLine("> 6 - Sair");
-
-            Console.WriteLine(">>Escolha a opção desejada\nR:");
+            Console.WriteLine(">    1 - Adição ");
+            Console.WriteLine(">    2 - Subtração");
+            Console.WriteLine(">    3 - Multiplicação");
+            Console.WriteLine(">    4 - Divisão");
+            Console.WriteLine(">    5 - Módulo");
+            Console.WriteLine(">    6 - Sair");
+            Console.Write(">> Escolha a opção desejada: ");
             op = int.Parse(Console.ReadLine());
-
             return op;
         }
         
+        static void LeNumeros(ref Complexos n1, ref Complexos n2){
+            double x, y;
+
+            Console.WriteLine("Informe a parte real do primeiro numero: ");
+            x = double.Parse(Console.ReadLine());
+            Console.WriteLine("Informe a parte imaginaria do primeiro numero: ");
+            y = double.Parse(Console.ReadLine());
+            n1.SetImag(x,y);
+
+            Console.WriteLine("Informe a parte real do segundo numero: ");
+            x = double.Parse(Console.ReadLine());
+            Console.WriteLine("Informe a parte imaginaria do segundo numero: ");
+            y = double.Parse(Console.ReadLine());
+            n2.SetImag(x, y);           
+        }
         static void Main(string[] args)
         {
+            int opcao = 0;
+            Complexos numero1 = new Complexos();
+            Complexos numero2 = new Complexos();
             
-            try
-            {
-                int opcao = Menu();
-                while (opcao != 6 )
-                {
-                   
+            try{
+                while (opcao != 6 ){
+                    opcao = Menu();
+                    switch (opcao){
+                        case 1:
+                            LeNumeros(ref numero1, ref numero2);
+                            Calculadora.Soma(numero1, numero2);
+                            break;
+
+                        case 2:
+                            LeNumeros(ref numero1, ref numero2);
+                            Calculadora.Subtracao(numero1, numero2);
+                            break;
+
+                        case 3:
+                            break;
+
+                        case 4:
+                            break;
+
+                        case 5:
+                            break;
+
+                        case 6:
+                            Console.WriteLine("Obrigado por usar a calculadora Imaginum!");
+                            break;
+
+                        default:
+                           break;
+                    }
+                    Console.WriteLine("Digite qualquer tecla para continuar...");
+                    Console.ReadKey(true);
+                    Console.Clear();
                 }
             }
             catch (Exception)
--- imaginum/imaginum/calculadora.cs
+++ imaginum/imaginum/calculadora.cs
@@ -6,8 +6,8 @@ using System.Threading.Tasks;
 
 namespace imaginum
 {
-    class calculadora{
-        public static void Soma(Complexos nmr1, Complexos nmr2){
+    class Calculadora{
+        public static void Soma(Complexos nmr1, Complexos nmr2){//soma o nmr1 pelo nmr2
             double somareal = nmr1.real + nmr2.real;
             double somaimag = nmr1.imaginario + nmr2.imaginario;
 
@@ -18,16 +18,15 @@ namespace imaginum
             }
         }
 
-        public static void Subtracao(Complexos nmr1, Complexos nmr2)
+        public static void Subtracao(Complexos nmr1, Complexos nmr2)//subtrai o nmr1 pelo nmr2
         {
-            double somareal = nmr1.real - nmr2.real;
-            double somaimag = nmr1.imaginario - nmr2.imaginario;
+            Complexos sub = new Complexos(nmr1.real - nmr2.real, nmr1.imaginario - nmr2.imaginario);
             
-            if (somaimag > 0){
-                Console.WriteLine("A subtracao dos numeros complexos é: " + somareal + "+" + somaimag + "i");
-            }else{
-                Console.WriteLine("A subtracao dos numeros complexos é: " + somareal + "-" + somaimag + "i");
-            }
+            //if (sub.imaginario > 0){
+                Console.WriteLine("A subtracao dos numeros complexos é: " + sub.ImprimeImaginario() );
+            //}else{
+                Console.WriteLine("A subtracao dos numeros complexos é: " + sub.ImprimeImaginario() );
+           //}
         }
 
         public static void Multiplicacao(Complexos nmr1, Complexos nmr2)//multiplica o nmr1 pelo nmr2
@@ -42,7 +41,8 @@ namespace imaginum
                 Console.WriteLine("O produto dos numeros complexos é: " + produto.real + "-" + produto.imaginario + "i");
             }
 
-        }public static void Divisao(Complexos nmr1, Complexos nmr2)//divide o nmr1 pelo nmr2
+        }
+        public static void Divisao(Complexos nmr1, Complexos nmr2)//divide o nmr1 pelo nmr2
         {
             Complexos divisao = new Complexos(((nmr1.real * nmr2.real) + (nmr1.imaginario * -nmr2.imaginario)) / (Math.Pow(nmr2.real, 2.0) + Math.Pow(-nmr2.imaginario, 2.0)),
                                                 ((nmr1.imaginario * nmr2.real) - (nmr1.real * -nmr2.imaginario)) / (Math.Pow(nmr2.real, 2.0) + Math.Pow(-nmr2.imaginario, 2.0)));
@@ -54,9 +54,10 @@ namespace imaginum
             }
         }
 
-        public static void Modulo(Complexos nmr1, Complexos nmr2){
-            Console.WriteLine("O modulo de " + nmr1.ImprimeImaginario() + "é " + nmr1.Modulo_Img()); 
-            Console.WriteLine("O modulo de " + nmr2.ImprimeImaginario() + "é " + nmr2.Modulo_Img());
+        public static void Modulo(Complexos nmr1, Complexos nmr2)//retorna o modulo de cada numero complexo
+        {
+            Console.WriteLine("O modulo de " + nmr1.ImprimeImaginario() + "é " + nmr1.Modulo_Img() ); 
+            Console.WriteLine("O modulo de " + nmr2.ImprimeImaginario() + "é " + nmr2.Modulo_Img() );
         }
     }
 }
