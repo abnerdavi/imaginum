8a83aad65b1870ea38b72c6849d83eb7bcebe10f
Abner Davi de Souza Miranda <abner_davi@ucl.br>
2021-10-09 08:49:26 -0300

    adição de métodos na classe Complexo

--- imaginum/imaginum/Complexos.cs
+++ imaginum/imaginum/Complexos.cs
@@ -4,19 +4,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace imaginum
-{
-    class Complexos
-    {
-       public double real { get; private set; }
-       public double imaginario { get; private set; }
+namespace imaginum{
+    class Complexos{
+        public double real { get; private set; }
+        public double imaginario { get; private set; }
 
-       public Complexos() {}
-       public Complexos(double _real, double _imaginario)
-       {
-           real = _real;
-           imaginario =_imaginario;
-       }
+        public Complexos() {}
+        public Complexos(double _real, double _imaginario){
+            real = _real;
+            imaginario =_imaginario;
+        }
 
+        public void SetImag(double rl, double img){
+            real = rl;
+            imaginario = img;
+        }
+        
+        public double Modulo_Img(){
+            double h;
+            h = Math.Sqrt( (Math.Pow(real,2.0) + Math.Pow(imaginario,2.0)) ) ;
+            return h;
+        }
     }
 }
