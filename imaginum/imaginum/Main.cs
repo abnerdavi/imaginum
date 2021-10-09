01b2bb2ed3ef84a130b6430905244ed8c67248cc
Abner Davi de Souza Miranda <abner_davi@ucl.br>
2021-10-09 11:41:22 -0300

    criação da classe Calculadora, com as 4 operações básicas

---
+++ imaginum/imaginum/Main.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace imaginum
+{
+    class main
+    {
+        static int Menu(){
+            int op;
+            Console.WriteLine("----------Bem vindo a Imaginum----------");
+            Console.WriteLine("---Calculadora de numeros imaginarios---");
+            Console.WriteLine("> 1 - Adição ");
+            Console.WriteLine("> 2 - Subtração");
+            Console.WriteLine("> 3 - Multiplicação");
+            Console.WriteLine("> 4 - Divisão");
+            Console.WriteLine("> 5 - Módulo");
+            Console.WriteLine(">>Escolha a opção desejada\nR:");
+            op = int.Parse(Console.ReadLine());
+
+            return op;
+        }
+
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+    }
+}
---
+++ imaginum/imaginum/calculadora.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace imaginum
+{
+    class calculadora{
+        public static void Soma(Complexos nmr1, Complexos nmr2){
+            double somareal = nmr1.real + nmr2.real;
+            double somaimag = nmr1.imaginario + nmr2.imaginario;
+
+            if (somaimag > 0){
+                Console.WriteLine("A soma dos numeros complexos é: " + somareal + "+" + somaimag + "i");
+            }else{
+                Console.WriteLine("A soma dos numeros complexos é: " + somareal + "-" + somaimag + "i");
+            }
+        }
+
+        public static void Subtracao(Complexos nmr1, Complexos nmr2)
+        {
+            double somareal = nmr1.real - nmr2.real;
+            double somaimag = nmr1.imaginario - nmr2.imaginario;
+            
+            if (somaimag > 0){
+                Console.WriteLine("A subtracao dos numeros complexos é: " + somareal + "+" + somaimag + "i");
+            }else{
+                Console.WriteLine("A subtracao dos numeros complexos é: " + somareal + "-" + somaimag + "i");
+            }
+        }
+
+        public static void Multiplicacao(Complexos nmr1, Complexos nmr2)
+        {
+            Complexos produto = new();
+
+            produto.SetImag( ( (nmr1.real * nmr2.real) - (nmr1.imaginario * nmr2.imaginario) ), ( (nmr1.real * nmr2.imaginario) + (nmr1.imaginario * nmr2.real) ) );
+
+            if (produto.imaginario > 0){
+                Console.WriteLine("O produto dos numeros complexos é: " + produto.real + "+" + produto.imaginario + "i");
+            }else{
+                Console.WriteLine("O produto dos numeros complexos é: " + produto.real + "-" + produto.imaginario + "i");
+            }
+
+        }public static void Divisao(Complexos nmr1, Complexos nmr2)
+        {
+            Complexos produto = new();
+
+            double real = ( (nmr1.real * nmr2.real) + (nmr1.imaginario * -nmr2.imaginario) ) / ( Math.Pow(nmr2.real,2.0)+Math.Pow(-nmr2.imaginario,2.0) );
+            double imaginaria = ( (nmr1.imaginario * nmr2.real) - (nmr1.real * -nmr2.imaginario) ) / ( Math.Pow(nmr2.real, 2.0) + Math.Pow(-nmr2.imaginario,2.0) );
+
+            produto.SetImag( real, imaginaria);
+
+            if (produto.imaginario > 0){
+                Console.WriteLine("A divisao entre os numeros complexos é: " + produto.real + "+" + produto.imaginario + "i");
+            }else{
+                Console.WriteLine("A divisao entre os numeros complexos é: " + produto.real + "-" + produto.imaginario + "i");
+            }
+        }
+
+    }
+}
